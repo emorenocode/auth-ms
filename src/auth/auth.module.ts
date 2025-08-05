2fc68cd974202a9119df049cad32a5abd5889ebf
Enrique Moreno <emorenocode@gmail.com>
2025-08-04 22:08:17 -0500

    feat: initialize auth microservice with NATS integration
    - Add .dockerignore to exclude unnecessary files from Docker context
    - Create .env.template for environment variable configuration
    - Add Dockerfile for building the Node.js application
    - Update package.json to include necessary dependencies for authentication
    - Add NATS configuration and environment variable handling
    - Implement AuthModule, AuthController, and AuthService for user authentication
    - Remove unused AppController and AppService
    - Update main.ts to bootstrap the application as a microservice using NATS

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { envs, NATS_SERVICE } from 'src/config';
+
+@Module({
+  controllers: [AuthController],
+  providers: [AuthService],
+  imports: [
+    ClientsModule.register([
+      {
+        name: NATS_SERVICE,
+        transport: Transport.NATS,
+        options: {
+          servers: envs.natsServers,
+        },
+      },
+    ]),
+  ],
+})
+export class AuthModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { envs } from 'src/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3003);
+  const logger = new Logger('Auth-MS');
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.NATS,
+      options: {
+        servers: envs.natsServers,
+      },
+    },
+  );
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  await app.listen();
+  logger.log(`Product MS is running on port: ${envs.port}`);
 }
 bootstrap();
